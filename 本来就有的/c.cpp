ff632b9c2cd03a0c4130449f2eecb70458e0f880
chevez3484524625 <1240572183@qq.com>
2019-02-02 12:18:20 +0800

    git

---
+++ 本来就有的/c.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout <<"comp up  and c++ me some time .";
+    cout << endl;
+    cout << "you won't regret it !"<<endl;
+    system("pause");
+    return 0 ;
+}
